933b608d5d23a340d496b901f2d8396b40070100
Marcos-Henriq <mh7505570@gmail.com>
2023-03-21 20:50:15 -0300

    adiciona dependencias de material design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Box } from '@material-ui/core'
+import { Box, createTheme,ThemeProvider } from '@material-ui/core'
+import HeaderLandingPage from './components/header';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6096BA",
+      light: "#A3CEF1",
+      dark: "#274C77",
+      contrastText: "#f6f6f6",
+
+      
+    }
+
+  }
+});
 
 function App() {
   return (
-    <Box>
-      Teste
-    </Box>
+    <ThemeProvider theme={theme}>
+      <HeaderLandingPage />
+    </ThemeProvider>
+
   );
 }
 
