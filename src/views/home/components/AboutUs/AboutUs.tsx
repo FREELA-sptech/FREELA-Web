bce6af3bbdd2f7667ce72646a451d47a42e22689
Paulo Filho <paulo.filho@genial.com.vc>
2023-03-29 18:06:07 -0300

    feat: create initial aboutus

--- src/views/home/components/AboutUs/AboutUs.tsx
+++ src/views/home/components/AboutUs/AboutUs.tsx
@@ -2,7 +2,37 @@ import "./style.scss"
 
 function AboutUs() {
     return (
-        <h1>AboutUs Component</h1>
+        <section className="container">
+            <div className="about-us-header">
+                <h1>O que é a Freela ?</h1>
+                <p className="about-us-header-subtitle">Nossa plataforma oferece uma ampla variedade de oportunidades de trabalho, desde tarefas simples a projetos mais complexos, em diversas áreas, como design gráfico, redação, programação, marketing digital, tradução, entre outras.</p>
+            </div>
+            <div className="about-us-body">
+                <div className="section-client">
+                    <div className="body">
+                        <div className="card text">
+                            <h2>SOU CLIENTE</h2>
+
+                            <h3>A FREELA oferece uma ampla seleção de profissionais qualificados e talentosos em diferentes áreas.</h3>
+
+                            <button>
+                                SOU CLIENTE
+                            </button>
+                        </div>
+                        <div className="card image">
+                            <img src="/assets/images/about-us-client.svg" alt="" />
+                        </div>
+                    </div>
+                    <div className="footer">
+
+                    </div>
+                </div>
+
+                <div className="section-freelance">
+
+                </div>
+            </div>
+        </section>
     );
 }
 
