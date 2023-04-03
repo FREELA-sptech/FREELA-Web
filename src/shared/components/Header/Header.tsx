91420404cb43f4b556a8646ea205a45502b8a449
Paulo Filho <paulo.filho@genial.com.vc>
2023-04-03 20:02:47 -0300

    feat: create header component

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,8 +1,15 @@
+import './style.scss'
+
 function Header() {
   return (
-    <div className="container">
-      <h1>Header Component</h1>
-    </div>
+    <header className="header-background">
+      <div className="container flex-row justify-space-between">
+        <h1 className="logo">FREELA</h1>
+        <li className='menu flex-row align-center'>
+          <ul className='subtitle'>Login</ul>
+        </li>
+      </div>
+    </header>
   );
 }
 export default Header;
