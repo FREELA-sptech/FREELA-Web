6a099164450e052a51a1a076372c74e352d5c5ee
Paulo Filho <paulo.filho@genial.com.vc>
2023-04-04 20:42:42 -0300

    fix: refactor banner section responsive

--- src/pages/home/components/Banner/Banner.tsx
+++ src/pages/home/components/Banner/Banner.tsx
@@ -1,6 +1,7 @@
 import "./style.scss"
 
 import ButtonBase from "../../../../shared/components/ButtonBase/ButtonBase";
+import { Col, Container, Row } from "react-bootstrap";
 
 function Banner() {
   const handleClickButton = () => {
@@ -8,27 +9,25 @@ function Banner() {
   }
 
   return (
-    <section className="banner-background flex-column">
-      <div className="container flex-row justify-center align-center flex-wrap">
-        <div className="text">
-          <h1 className="big-title">
-            ONDE <span>QUEM</span> PRECISA <br />
-            ACHA <span>QUEM</span> FAZ.
-          </h1>
-          <p className="summary">
-            Conectamos talentosos profissionais independentes com empresas
-            e indivíduos em busca de ajuda para projetos de curto ou longo prazo.
-          </p>
-          <ButtonBase onClick={handleClickButton} buttonType="secundary-standart" label="Quero Começar" />
-        </div>
-        <div className="image">
-          <img src="/assets/images/banner-icon.svg" alt="duas setas para direita" />
-        </div>
-        <div className="show-more flex-column align-center">
-          <img src="/assets/icons/mouse.svg" alt="mouse" />
-          <h3 className="subtitle">VER MAIS</h3>
-        </div>
-      </div>
+    <section className="banner-background">
+      <Container>
+        <Row>
+          <Col xl={6} className="banner-card">
+            <h1 className="big-title banner-title">
+              ONDE <span>QUEM</span> PRECISA <br />
+              ACHA <span>QUEM</span> FAZ.
+            </h1>
+            <p className="summary banner-summary">
+              Conectamos talentosos profissionais independentes com empresas
+              e indivíduos em busca de ajuda para projetos de curto ou longo prazo.
+            </p>
+            <ButtonBase onClick={handleClickButton} buttonType="secundary-standart" label="Quero Começar" />
+          </Col>
+          <Col xl={6} className="banner-card image">
+            <img src="/assets/images/banner-icon.svg" alt="duas setas para direita" />
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 }
