cdde9f8f449e72fcee0eaa404bfd789d85231f85
Paulo Filho <paulo.filho@genial.com.vc>
2023-04-03 21:21:58 -0300

    feat: create banner component

--- src/pages/home/components/Banner/Banner.tsx
+++ src/pages/home/components/Banner/Banner.tsx
@@ -3,20 +3,30 @@ import "./style.scss"
 import ButtonBase from "../../../../shared/components/ButtonBase/ButtonBase";
 
 function Banner() {
-  const handleButton = () => {
-    alert("PAULIN");
+  const handleClickButton = () => {
+
   }
 
   return (
-    <section className="container-banner">
-      <div className="container">
-        <div>
-          <h1>ONDE QUEM PRECISA ACHA QUEM FAZ.</h1>
-          <h3>Conectamos talentosos profissionais independentes com empresas e indivíduos em busca de ajuda para projetos de curto ou longo prazo.</h3>
-          <ButtonBase />
+    <section className="banner-background flex-column">
+      <div className="container flex-row justify-center align-center flex-wrap">
+        <div className="text">
+          <h1 className="big-title">
+            ONDE <span>QUEM</span> PRECISA <br />
+            ACHA <span>QUEM</span> FAZ.
+          </h1>
+          <p className="summary">
+            Conectamos talentosos profissionais independentes com empresas
+            e indivíduos em busca de ajuda para projetos de curto ou longo prazo.
+          </p>
+          <ButtonBase onClick={handleClickButton} buttonType="secundary-standart" label="Quero Começar" />
         </div>
-        <div>
-          <img src="/assets/img/img-banner.svg" alt="" />
+        <div className="image">
+          <img src="/assets/images/banner-icon.svg" alt="duas setas para direita" />
+        </div>
+        <div className="show-more flex-column align-center">
+          <img src="/assets/icons/mouse.svg" alt="mouse" />
+          <h3 className="subtitle">VER MAIS</h3>
         </div>
       </div>
     </section>
