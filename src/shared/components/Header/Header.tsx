e66366073deaba8d4a532f08ac6ed161e4a0396f
Paulo Filho <paulo.filho@genial.com.vc>
2023-04-03 21:13:23 -0300

    feat: create routes navigate

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,13 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import ButtonBase from '../ButtonBase/ButtonBase';
 import './style.scss'
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleTransition = (path: string) => {
+    navigate(path)
+  }
+
   return (
     <header className="header-background">
       <div className="container flex-row justify-space-between">
         <h1 className="logo">FREELA</h1>
-        <li className='menu flex-row align-center'>
-          <ul className='subtitle'>Login</ul>
-        </li>
+        <ul className='menu flex-row align-center'>
+          <li>
+            <ButtonBase onClick={() => handleTransition("/login")} buttonType='tertiary-text' label='login' />
+          </li>
+          <li>
+            <ButtonBase onClick={() => handleTransition("/cadastro")} buttonType='secundary-standart' label='cadastre-se' />
+          </li>
+        </ul>
       </div>
     </header>
   );
