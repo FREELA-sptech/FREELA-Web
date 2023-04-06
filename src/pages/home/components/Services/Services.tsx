9910949ed2f289a9a946cc5f5c5e4dc1ff7edd38
Marcos-Henriq <mh7505570@gmail.com>
2023-04-06 20:32:35 -0300

    seção de serviços finalizada

--- src/pages/home/components/Services/Services.tsx
+++ src/pages/home/components/Services/Services.tsx
@@ -1,46 +1,50 @@
+import { Col, Container, Row } from "react-bootstrap";
 import "./style.scss"
 
 function Services() {
     return (
-        <section className="container-fluid services-background">
-            <div className="container flex-row justify-content-between align-itens-center flex-wrap">
-                <div className="col-6 left-side align-center justify-content-between">
-                    <div className="title">
-                        <h1>skalksllasskal</h1>
-                        <h1>skalksllasskal</h1>
+        <Container fluid className="d-flex services-background align-items-center flex-column justify-content-center">
+            <Container className="d-flex justify-content-between flex-wrap align-items-center">
+                <Col md={5} xs={12} className="flex-column left-side ">
+                    <div className="services-header">
+                        <h2 className="services-topic text-uppercase">Descubra</h2>
+                        <h1 className="services-title text-uppercase">Principais Serviços.</h1>
                     </div>
                     <div className="content">
-                    Nossa plataforma oferece uma ampla variedade de serviços, desde tarefas simples a projetos mais complexos, em diversas áreas, como design gráfico, redação, programação, marketing digital, tradução, entre outras.
+                        <p className="services-content-text">Nossa plataforma oferece uma ampla variedade de serviços, desde tarefas simples a projetos mais complexos, em diversas áreas, como design gráfico, redação, programação, marketing digital, tradução, entre outras.</p>
                     </div>
-                </div>
-                <div className="col-6 right-side justify-content-between flex-row flex-wrap align-center">
-                    <div className="card-service">
-                        <img src="" alt="" />
-                        <h3>Programação</h3>
-                    </div>
-                    <div className="card-service">
-                        <img src="" alt="" />
-                        <h3>Programação</h3>
-                    </div>
-                    <div className="card-service">
-                        <img src="" alt="" />
-                        <h3>Programação</h3>
-                    </div>
-                    <div className="card-service">
-                        <img src="" alt="" />
-                        <h3>Programação</h3>
-                    </div>
-                    <div className="card-service">
-                        <img src="" alt="" />
-                        <h3>Programação</h3>
-                    </div>
-                    <div className="card-service">
-                        <img src="" alt="" />
-                        <h3>Programação</h3>
-                    </div>
-                </div>
-            </div>
-        </section>
+                    <hr />
+                </Col>
+                <Col md={7} xs={12} className="d-flex left-side flex-wrap">
+                    <Row className="d-flex justify-content-center flex-wrap container-cards">
+                        <Col lg={5} md={12} className="card-service d-flex flex-wrap align-items-center">
+                            <img src="/assets/icons/code-2.svg" alt="" />
+                            <h5 className="title">Programação</h5>
+                        </Col>
+                        <Col lg={5} md={12} className="card-service d-flex flex-wrap align-items-center">
+                            <img src="/assets/icons/paintbrush.svg" alt="" />
+                            <h5 className="title">Design</h5>
+                        </Col>
+                        <Col lg={5} md={12} className="card-service d-flex flex-wrap align-items-center">
+                            <img src="/assets/icons/languages.svg" alt="" />
+                            <h5 className="title">Tradução</h5>
+                        </Col>
+                        <Col lg={5} md={12} className="card-service d-flex flex-wrap align-items-center">
+                            <img src="/assets/icons/book-open.svg" alt="" />
+                            <h5 className="title">Redação</h5>
+                        </Col>
+                        <Col lg={5} md={12} className="card-service d-flex flex-wrap align-items-center">
+                            <img src="/assets/icons/palette.svg" alt="" />
+                            <h5 className="title">Artes</h5>
+                        </Col>
+                        <Col lg={5} md={12} className="card-service d-flex flex-wrap align-items-center">
+                            <img src="/assets/icons/paintbrush.svg" alt="" />
+                            <h5 className="title">Marketing</h5>
+                        </Col>
+                    </Row>
+                </Col>
+            </Container>
+        </Container>
     )
 }
 
