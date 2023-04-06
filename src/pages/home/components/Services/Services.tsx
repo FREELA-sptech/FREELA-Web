958205bffb29a83e54ce706e0b1542c7a2602c52
Marcos-Henriq <mh7505570@gmail.com>
2023-04-06 01:01:11 -0300

    adiciona section dos principais serviços

---
+++ src/pages/home/components/Services/Services.tsx
@@ -0,0 +1,24 @@
+import "./style.scss"
+
+function Services() {
+    return (
+        <section className="container-fluid services-background">
+            <div className="row container flex-row justify-content-between align-itens-center flex-wrap">
+                <div className="col-6 left-side">
+                    <div className="title">
+                        <h1>skalksllasskal</h1>
+                        <h1>skalksllasskal</h1>
+                    </div>
+                    <div className="content">
+                    Nossa plataforma oferece uma ampla variedade de serviços, desde tarefas simples a projetos mais complexos, em diversas áreas, como design gráfico, redação, programação, marketing digital, tradução, entre outras.
+                    </div>
+                </div>
+                <div className="col-6 right-side">
+                    <h1>texte</h1>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Services;
