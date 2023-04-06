ea0514a3b7ecadf3e9e3572ecea430ae03fdf55c
Marcos-Henriq <mh7505570@gmail.com>
2023-04-06 01:27:39 -0300

    divide layout em dois lados

--- src/pages/home/components/Services/Services.tsx
+++ src/pages/home/components/Services/Services.tsx
@@ -3,8 +3,8 @@ import "./style.scss"
 function Services() {
     return (
         <section className="container-fluid services-background">
-            <div className="row container flex-row justify-content-between align-itens-center flex-wrap">
-                <div className="col-6 left-side">
+            <div className="container flex-row justify-content-between align-itens-center flex-wrap">
+                <div className="col-6 left-side align-center justify-content-between">
                     <div className="title">
                         <h1>skalksllasskal</h1>
                         <h1>skalksllasskal</h1>
@@ -13,8 +13,31 @@ function Services() {
                     Nossa plataforma oferece uma ampla variedade de serviços, desde tarefas simples a projetos mais complexos, em diversas áreas, como design gráfico, redação, programação, marketing digital, tradução, entre outras.
                     </div>
                 </div>
-                <div className="col-6 right-side">
-                    <h1>texte</h1>
+                <div className="col-6 right-side justify-content-between flex-row flex-wrap align-center">
+                    <div className="card-service">
+                        <img src="" alt="" />
+                        <h3>Programação</h3>
+                    </div>
+                    <div className="card-service">
+                        <img src="" alt="" />
+                        <h3>Programação</h3>
+                    </div>
+                    <div className="card-service">
+                        <img src="" alt="" />
+                        <h3>Programação</h3>
+                    </div>
+                    <div className="card-service">
+                        <img src="" alt="" />
+                        <h3>Programação</h3>
+                    </div>
+                    <div className="card-service">
+                        <img src="" alt="" />
+                        <h3>Programação</h3>
+                    </div>
+                    <div className="card-service">
+                        <img src="" alt="" />
+                        <h3>Programação</h3>
+                    </div>
                 </div>
             </div>
         </section>
