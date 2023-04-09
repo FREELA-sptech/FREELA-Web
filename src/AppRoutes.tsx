2dc9cd8d3f94881160182fc93fac074dbbe12080
Tiago Ferreira <tiago.silva@sptech.school>
2023-04-08 21:37:12 -0300

    primeira versao da tela de login

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,12 +2,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Header from './shared/components/Header/Header'
 import Home from './pages/home/Home'
+import Login from './pages/Login/Login'
+
+
+
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Header></Header>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>
 )
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,15 @@
+import CardLogin from "./components/CardLogin/CardLogin";
+import "./style.scss"
+import React from "react";
+
+
+function Login() {
+    return (
+      <>
+          <CardLogin />   
+      </>
+    );
+  }
+  
+  export default Login
+  
