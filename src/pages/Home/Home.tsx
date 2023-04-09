082d6bd35ecdf6444541b1078ed445bfa98f9597
Paulo Filho <paulo.filho@genial.com.vc>
2023-04-09 02:01:35 -0300

    fix: correction header

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,36 +4,39 @@ import './style.scss'
 
 function Home() {
   return (
-    <Container>
-      <Row className="p-3 d-flex gap-2">
-        <Figure className="home-icon-background d-flex justify-content-center align-items-center m-0">
-          <Figure.Image
-            width='40px'
-            height='40px'
-            alt="icone filtro"
-            src="assets/icons/filter.svg"
-          />
-        </Figure>
-        <Form.Control className="primary-input w-auto flex-grow-1" placeholder="Busque Aqui" />
-      </Row>
-      <Row className="d-flex">
-        <Col xs={12} md={6} lg={4} className="p-3">
-          <ServicesAvailableCard />
-        </Col>
-        <Col xs={12} md={6} lg={4} className="p-3">
-          <ServicesAvailableCard />
-        </Col>
-        <Col xs={12} md={6} lg={4} className="p-3">
-          <ServicesAvailableCard />
-        </Col>
-        <Col xs={12} md={6} lg={4} className="p-3">
-          <ServicesAvailableCard />
-        </Col>
-        <Col xs={12} md={6} lg={4} className="p-3">
-          <ServicesAvailableCard />
-        </Col>
-      </Row>
-    </Container>
+    <section className="home-background">
+      <Container>
+        <Row className="p-3 d-flex gap-2">
+          <Figure className="home-icon-background d-flex justify-content-center align-items-center">
+            <Figure.Image
+              width='40px'
+              height='40px'
+              alt="icone filtro"
+              src="assets/icons/filter.svg"
+              className="m-0"
+            />
+          </Figure>
+          <Form.Control className="primary-input w-auto flex-grow-1" placeholder="Busque Aqui" />
+        </Row>
+        <Row className="d-flex">
+          <Col xs={12} md={6} lg={4} className="p-3">
+            <ServicesAvailableCard />
+          </Col>
+          <Col xs={12} md={6} lg={4} className="p-3">
+            <ServicesAvailableCard />
+          </Col>
+          <Col xs={12} md={6} lg={4} className="p-3">
+            <ServicesAvailableCard />
+          </Col>
+          <Col xs={12} md={6} lg={4} className="p-3">
+            <ServicesAvailableCard />
+          </Col>
+          <Col xs={12} md={6} lg={4} className="p-3">
+            <ServicesAvailableCard />
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 }
 
