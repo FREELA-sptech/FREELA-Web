87ecb640f366d92da93c43c46e08b7646753c50d
Paulo Filho <paulo.filho@genial.com.vc>
2023-04-11 20:02:47 -0300

    feat: create filters card

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,38 +1,48 @@
-import { Col, Container, Figure, Form, Row } from "react-bootstrap";
+import { Accordion, Card, Col, Container, Figure, Form, Row, useAccordionButton } from "react-bootstrap";
 import ServicesAvailableCard from "./components/ServicesAvailableCard/ServicesAvailableCard";
 import './style.scss'
+import { useState } from "react";
+import FiltersCard from "./components/FiltersCard/FiltersCard";
 
 function Home() {
   return (
     <section className="home-background">
       <Container>
-        <Row className="p-3 d-flex gap-2">
-          <Figure className="home-icon-background d-flex justify-content-center align-items-center">
-            <Figure.Image
-              width='40px'
-              height='40px'
-              alt="icone filtro"
-              src="assets/icons/filter.svg"
-              className="m-0"
-            />
-          </Figure>
-          <Form.Control className="primary-input w-auto flex-grow-1" placeholder="Busque Aqui" />
-        </Row>
         <Row className="d-flex">
-          <Col xs={12} md={6} lg={4} className="p-3">
-            <ServicesAvailableCard />
+          <Col lg={3}>
+            <FiltersCard />
           </Col>
-          <Col xs={12} md={6} lg={4} className="p-3">
-            <ServicesAvailableCard />
-          </Col>
-          <Col xs={12} md={6} lg={4} className="p-3">
-            <ServicesAvailableCard />
-          </Col>
-          <Col xs={12} md={6} lg={4} className="p-3">
-            <ServicesAvailableCard />
-          </Col>
-          <Col xs={12} md={6} lg={4} className="p-3">
-            <ServicesAvailableCard />
+          <Col lg={9} className="px-3 d-flex flex-column gap-2">
+            <Row className="px-3">
+              {/* <Figure className="home-icon-background d-flex justify-content-center align-items-center">
+                <Figure.Image
+                  width='40px'
+                  height='40px'
+                  alt="icone filtro"
+                  src="assets/icons/filter.svg"
+                  className="m-0"
+                />
+              </Figure> */}
+              <Form.Control className="primary-input w-auto flex-grow-1" placeholder="Busque Aqui" />
+            </Row>
+
+            <Row className="d-flex">
+              <Col xs={12} md={6} lg={4} className="p-3">
+                <ServicesAvailableCard />
+              </Col>
+              <Col xs={12} md={6} lg={4} className="p-3">
+                <ServicesAvailableCard />
+              </Col>
+              <Col xs={12} md={6} lg={4} className="p-3">
+                <ServicesAvailableCard />
+              </Col>
+              <Col xs={12} md={6} lg={4} className="p-3">
+                <ServicesAvailableCard />
+              </Col>
+              <Col xs={12} md={6} lg={4} className="p-3">
+                <ServicesAvailableCard />
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
---
+++ src/pages/Home/components/FiltersCard/FiltersCard.tsx
@@ -0,0 +1,75 @@
+import { Accordion, Figure, Form, ListGroup } from "react-bootstrap";
+import './style.scss'
+
+function FiltersCard() {
+  return (
+    <Accordion className="d-flex flex-column gap-2" alwaysOpen>
+      <Accordion.Item eventKey="0" className="filters-card-background">
+        <Accordion.Header className="filters-card-header">
+          <Figure.Image
+            width='40px'
+            height='40px'
+            alt="icone filtro"
+            src="assets/icons/ordenation.svg"
+            className="m-0"
+          />
+          Ordenar
+        </Accordion.Header>
+        <Accordion.Body>
+          <ListGroup>
+            <ListGroup.Item className="d-flex justify-content-between border-0">
+              Mais Recente
+              <Form.Check className="checkbox" aria-label="option 1" />
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex justify-content-between border-0">
+              Mais Recente
+              <Form.Check className="checkbox" aria-label="option 1" />
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex justify-content-between border-0">
+              Mais Recente
+              <Form.Check className="checkbox" aria-label="option 1" />
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex justify-content-between border-0">
+              Mais Recente
+              <Form.Check className="checkbox" aria-label="option 1" />
+            </ListGroup.Item>
+          </ListGroup>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1" className="filters-card-background">
+        <Accordion.Header className="filters-card-header">
+          <Figure.Image
+            width='40px'
+            height='40px'
+            alt="icone filtro"
+            src="assets/icons/ordenation.svg"
+            className="m-0"
+          />
+          Ordenar
+        </Accordion.Header>
+        <Accordion.Body>
+          <ListGroup>
+            <ListGroup.Item className="d-flex justify-content-between border-0">
+              Mais Recente
+              <Form.Check className="checkbox" aria-label="option 1" />
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex justify-content-between border-0">
+              Mais Recente
+              <Form.Check className="checkbox" aria-label="option 1" />
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex justify-content-between border-0">
+              Mais Recente
+              <Form.Check className="checkbox" aria-label="option 1" />
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex justify-content-between border-0">
+              Mais Recente
+              <Form.Check className="checkbox" aria-label="option 1" />
+            </ListGroup.Item>
+          </ListGroup>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
+
+export default FiltersCard
