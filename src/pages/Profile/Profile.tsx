f8fd0d7919691c9db3263235ecb7041f5d51b9c2
Paulo Filho <paulo.filho@genial.com.vc>
2023-05-10 20:45:13 -0300

    feat: create page perfil

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -6,25 +6,58 @@ import FreelancerProfileCard from "../../shared/components/FreelancerProfileCard
 import CardProfile from "./components/CardProfile/CardProfile";
 import { OrderUpdate } from "../Order/OrderUpdate/OrderUpdate";
 import { Portfólio } from "./components/Portfolio/Portfolio";
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
 
 function Profile() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
   return (
     <section className="home-background">
       <Container>
-        <Row className="d-flex pt-3">
-          <Col lg={5}>
+        <Row className="d-flex py-3">
+          <Col lg={12}>
             <CardProfile />
           </Col>
-          <Col lg={7} className="px-3 pt-lg-0 pt-4 d-flex flex-column gap-2">
+          <Col lg={12}>
+            <Box
+              className="px-5"
+              sx={{
+                backgroundColor: 'white',
+                borderRadius: '0 0 16px 16px'
+              }}
+            >
+              <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                  <TabList onChange={handleChange} aria-label="lab API tabs example">
+                    <Tab label="Item One" value="1" />
+                    <Tab label="Item Two" value="2" />
+                  </TabList>
+                </Box>
+                <TabPanel value="1" sx={{backgroundColor: 'red'}}>Item One</TabPanel>
+                <TabPanel value="2">Item Two</TabPanel>
+              </TabContext>
+            </Box>
+          </Col>
+
+          {/* <Col lg={7} className="px-3 pt-lg-0 pt-4 d-flex flex-column gap-2">
             <button onClick={() => setShow(true)} className="buttonBase primary-standart">Adicionar Projeto</button>
             <Row className="d-flex">
 
             </Row>
-          </Col>
+          </Col> */}
         </Row>
       </Container>
       <Modal
@@ -46,3 +79,4 @@ function Profile() {
 }
 
 export default Profile
+// eu sou gay
--- src/pages/Profile/components/CardProfile/CardProfile.tsx
+++ src/pages/Profile/components/CardProfile/CardProfile.tsx
@@ -1,107 +1,194 @@
 import { Button, Card, Figure, OverlayTrigger, Popover, Row, Tooltip } from "react-bootstrap";
 import "./style.scss"
 import ButtonBase from "../../../../shared/components/ButtonBase/ButtonBase";
+import { Avatar, Box } from "@mui/material";
+import { deepOrange, deepPurple } from '@mui/material/colors';
+import StarIcon from '@mui/icons-material/Star';
 
 
 function CardProfile() {
   return (
-    <Card className="card-profile-background b-radius overflow-hidden">
-      <Card.Body className="px-4 py-4">
-        <Figure className="d-flex flex-column align-items-center gap-2 m-0" style={{ padding: '1px' }}>
-          <Row style={{
-            borderRadius: '99%',
-            padding: '3px',
-            margin: 0,
-            width: '150px',
-            height: '150px',
-            backgroundColor: 'var(--contrast-background-color)',
-            overflow: 'hidden'
-          }}>
+    <Box className="pb-4 overflow-hidden"
+      sx={{
+        backgroundColor: 'white',
+        borderRadius: '16px 16px 0 0'
+      }}
+    >
+      <Box className="position-relative d-flex flex-row gap-4" sx={{ height: '200px', backgroundColor: 'blue' }}>
+        <Avatar
+          className="position-absolute"
+          sx={{
+            width: 150,
+            height: 150,
+            bgcolor: deepOrange[500],
+            border: '4px solid white',
+            bottom: '-75px',
+            left: '50px'
+          }}
+          alt="Remy Sharp"
+          src="https://www.ogol.com.br/img/jogadores/58/976658_med__20230131161334_cassio.png"
+        />
+      </Box>
+      <Box className="d-flex flex-column ps-5" sx={{ paddingTop: '100px' }}>
+        <Box className="d-flex align-items-end gap-2">
+          <span className="f-30 f-inter dark-contrast-color fw-bold">Cassio Ramos</span>
+          <Box className="h-100 d-flex flex-row align-items-center pb-2">
+            <span className="aditional-color f-16">3.9</span>
+            <StarIcon color="primary" sx={{ fontSize: '12px' }} />
+          </Box>
+        </Box>
+        <span className="text-color fw-normal f-16 f-inter">São Paulo, SP</span>
+        <span className="py-3 f-poppings aditional-color f-16">"Sou um jogador que está sempre buscando evoluir e aprimorar minhas habilidades, seja através de treinos específicos ou análise de jogos."</span>
+        <h1 className="text-color f-18 f-inter fw-bold mt-2">Minhas Especialidades</h1>
+        <Box className="w-auto d-flex gap-2">
+          <OverlayTrigger
+            trigger={["hover", "focus"]}
+            key='teste'
+            placement='top'
+            overlay={
+              <h1 className="tooltip b-radius px-3 mb-1 fw-bold">Tradução</h1>
+            }
+          >
             <Figure.Image
-              width='100%'
-              height='100%'
-              style={{ padding: 0 }}
+              width='40px'
+              height='40px'
               alt="dollar"
-              src="https://www.ogol.com.br/img/jogadores/58/976658_med__20230131161334_cassio.png"
-              className="m-0"
+              src="/assets/icons/tradution.svg"
+              className=""
             />
-          </Row>
-          <Figure.Caption className="w-100 d-flex align-items-center justify-content-center pt-3">
-            <span className="f-30 f-inter dark-contrast-color fw-bold">Cassio Ramos</span>
-          </Figure.Caption>
-        </Figure>
-        <Row className="d-flex justify-content-center mt-1 text-center">
-          <Figure className="d-flex align-items-center justify-content-center m-0">
+          </OverlayTrigger>
+          <OverlayTrigger
+            trigger={["hover", "focus"]}
+            key='teste'
+            placement='top'
+            overlay={
+              <h1 className="tooltip b-radius px-3 mb-1 fw-bold">Tradução</h1>
+            }
+          >
             <Figure.Image
-              width='15px'
-              height='15px'
+              width='40px'
+              height='40px'
               alt="dollar"
-              src="/assets/icons/location.svg"
-              className="m-0 me-2"
+              src="/assets/icons/tradution.svg"
+              className=""
             />
-            <Figure.Caption className="text-color fw-semibold f-16 f-inter">
-              São Paulo, SP
-            </Figure.Caption>
-          </Figure>
-        </Row>
-        <Row className="d-flex justify-content-between my-3 text-center">
-          <span className="f-poppings aditional-color f-16">"Sou um jogador que está sempre buscando evoluir e aprimorar minhas habilidades, seja através de treinos específicos ou análise de jogos."</span>
-        </Row>
-        <Row className="d-flex justify-content-between my-3 text-center">
-          <h1 className="text-color f-18 f-inter fw-bold mt-2">Minhas Especialidades</h1>
-          <div className="d-flex justify-content-center gap-2 pt-1">
-            <OverlayTrigger
-              trigger={["hover", "focus"]}
-              key='teste'
-              placement='top'
-              overlay={
-                <h1 className="tooltip b-radius px-3 mb-1 fw-bold">Tradução</h1>
-              }
-            >
-              <Figure.Image
-                width='40px'
-                height='40px'
-                alt="dollar"
-                src="/assets/icons/tradution.svg"
-                className=""
-              />
-            </OverlayTrigger>
-          </div>
-        </Row>
-        <Row className="d-flex justify-content-between px-5 w-100 m-0">
-        <Row className="d-flex w-auto justify-content-start align-items-start text-start flex-column">
-            <h2 className="aditional-color f-poppings f-14 fw-normal w-auto">Avalição:</h2>
-            <Figure className="d-flex align-items-center gap-2 w-auto m-0 p-0">
-              <Figure.Image
-                width='30px'
-                height='30px'
-                alt="calendario"
-                src="/assets/icons/star.svg"
-                className="m-0"
-              />
-              <Figure.Caption className="d-flex flex-column">
-                <span className="f-roboto f-22 text-color fw-bold">4.9</span>
-              </Figure.Caption>
-            </Figure>
-          </Row>
-          <Row className="d-flex w-auto justify-content-start align-items-start text-start flex-column">
-            <h2 className="aditional-color f-poppings f-14 fw-normal w-auto">Negócios:</h2>
-            <Figure className="d-flex align-items-center gap-2 w-auto m-0 p-0">
-              <Figure.Image
-                width='30px'
-                height='30px'
-                alt="calendario"
-                src="/assets/icons/handshake.svg"
-                className="m-0"
-              />
-              <Figure.Caption className="d-flex flex-column">
-                <span className="f-roboto f-22 text-color fw-bold">300</span>
-              </Figure.Caption>
-            </Figure>
-          </Row>
-        </Row>
-      </Card.Body>
-    </Card>
+          </OverlayTrigger>
+          <OverlayTrigger
+            trigger={["hover", "focus"]}
+            key='teste'
+            placement='top'
+            overlay={
+              <h1 className="tooltip b-radius px-3 mb-1 fw-bold">Tradução</h1>
+            }
+          >
+            <Figure.Image
+              width='40px'
+              height='40px'
+              alt="dollar"
+              src="/assets/icons/tradution.svg"
+              className=""
+            />
+          </OverlayTrigger>
+        </Box>
+      </Box>
+    </Box >
+    // <Card className="card-profile-background b-radius overflow-hidden">
+    //   <Card.Body className="px-4 py-4">
+    //     <Figure className="d-flex flex-column align-items-center gap-2 m-0" style={{ padding: '1px' }}>
+    //       <Row style={{
+    //         borderRadius: '99%',
+    //         padding: '3px',
+    //         margin: 0,
+    //         width: '150px',
+    //         height: '150px',
+    //         backgroundColor: 'var(--contrast-background-color)',
+    //         overflow: 'hidden'
+    //       }}>
+    //         <Figure.Image
+    //           width='100%'
+    //           height='100%'
+    //           style={{ padding: 0 }}
+    //           alt="dollar"
+    //           src="https://www.ogol.com.br/img/jogadores/58/976658_med__20230131161334_cassio.png"
+    //           className="m-0"
+    //         />
+    //       </Row>
+    //       <Figure.Caption className="w-100 d-flex align-items-center justify-content-center pt-3">
+    //         <span className="f-30 f-inter dark-contrast-color fw-bold">Cassio Ramos</span>
+    //       </Figure.Caption>
+    //     </Figure>
+    //     <Row className="d-flex justify-content-center mt-1 text-center">
+    //       <Figure className="d-flex align-items-center justify-content-center m-0">
+    //         <Figure.Image
+    //           width='15px'
+    //           height='15px'
+    //           alt="dollar"
+    //           src="/assets/icons/location.svg"
+    //           className="m-0 me-2"
+    //         />
+    //         <Figure.Caption className="text-color fw-semibold f-16 f-inter">
+    //           São Paulo, SP
+    //         </Figure.Caption>
+    //       </Figure>
+    //     </Row>
+    //     <Row className="d-flex justify-content-between my-3 text-center">
+    //       <span className="f-poppings aditional-color f-16">"Sou um jogador que está sempre buscando evoluir e aprimorar minhas habilidades, seja através de treinos específicos ou análise de jogos."</span>
+    //     </Row>
+    //     <Row className="d-flex justify-content-between my-3 text-center">
+    //       <h1 className="text-color f-18 f-inter fw-bold mt-2">Minhas Especialidades</h1>
+    //       <div className="d-flex justify-content-center gap-2 pt-1">
+    //         <OverlayTrigger
+    //           trigger={["hover", "focus"]}
+    //           key='teste'
+    //           placement='top'
+    //           overlay={
+    //             <h1 className="tooltip b-radius px-3 mb-1 fw-bold">Tradução</h1>
+    //           }
+    //         >
+    //           <Figure.Image
+    //             width='40px'
+    //             height='40px'
+    //             alt="dollar"
+    //             src="/assets/icons/tradution.svg"
+    //             className=""
+    //           />
+    //         </OverlayTrigger>
+    //       </div>
+    //     </Row>
+    //     <Row className="d-flex justify-content-between px-5 w-100 m-0">
+    //     <Row className="d-flex w-auto justify-content-start align-items-start text-start flex-column">
+    //         <h2 className="aditional-color f-poppings f-14 fw-normal w-auto">Avalição:</h2>
+    //         <Figure className="d-flex align-items-center gap-2 w-auto m-0 p-0">
+    //           <Figure.Image
+    //             width='30px'
+    //             height='30px'
+    //             alt="calendario"
+    //             src="/assets/icons/star.svg"
+    //             className="m-0"
+    //           />
+    //           <Figure.Caption className="d-flex flex-column">
+    //             <span className="f-roboto f-22 text-color fw-bold">4.9</span>
+    //           </Figure.Caption>
+    //         </Figure>
+    //       </Row>
+    //       <Row className="d-flex w-auto justify-content-start align-items-start text-start flex-column">
+    //         <h2 className="aditional-color f-poppings f-14 fw-normal w-auto">Negócios:</h2>
+    //         <Figure className="d-flex align-items-center gap-2 w-auto m-0 p-0">
+    //           <Figure.Image
+    //             width='30px'
+    //             height='30px'
+    //             alt="calendario"
+    //             src="/assets/icons/handshake.svg"
+    //             className="m-0"
+    //           />
+    //           <Figure.Caption className="d-flex flex-column">
+    //             <span className="f-roboto f-22 text-color fw-bold">300</span>
+    //           </Figure.Caption>
+    //         </Figure>
+    //       </Row>
+    //     </Row>
+    //   </Card.Body>
+    // </Card>
   );
 }
 
