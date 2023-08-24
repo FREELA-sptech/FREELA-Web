531be8661ea5a5a04c40f1cecfc89036d2585631
Paulo-Enrique-Santos <paulo.santosfo@sptech.school>
2023-08-24 18:58:20 -0300

    fix: test add header

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,7 +6,7 @@ import Index from './pages/Index/Index'
 import Cadastro from './pages/Cadastro/Cadastro'
 import Home from './pages/Home/Home'
 import Profile from './pages/Profile/Profile'
-import  OrderDetails  from './pages/Order/OrderDetails/OrderDetails'
+import OrderDetails from './pages/Order/OrderDetails/OrderDetails'
 import { CreateOrder } from './pages/Order/CreateOrder/CreateOrder'
 import Proposta from './pages/Proposta/Proposta'
 import AuthProvider from './core/Auth/AuthContext'
@@ -18,6 +18,7 @@ import Backoffice from './pages/Backoffice/Backoffice'
 const AppRoutes = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<RedirectProvider element={<Index />} path='/home' />} />
         <Route path="/login" element={<RedirectProvider element={<Login />} path='/home' />} />
@@ -28,7 +29,7 @@ const AppRoutes = () => {
         <Route path="/create-order" element={<AuthProvider element={<CreateOrder />} />} />
         <Route path="/proposta" element={<AuthProvider element={<Proposta />} />} />
         <Route path="/chat" element={<AuthProvider element={<Chat />} />} />
-        <Route path="/backoffice" element={<Backoffice/>} />
+        <Route path="/backoffice" element={<Backoffice />} />
       </Routes>
     </BrowserRouter>
   )
