393e1f4f676b5b39f6dd7a69cefe42f43b99758f
Paulo Filho <paulo.filho@genial.com.vc>
2023-09-11 21:54:27 -0300

    fix: novas correções

--- src/pages/Chat/components/ListConversations/ListConversation.tsx
+++ src/pages/Chat/components/ListConversations/ListConversation.tsx
@@ -3,23 +3,54 @@ import "./style.scss";
 import Conversation from "../Conversation/Conversation";
 
 export type Props = {
-  chatData: any,
-  handleGetMessagesData: (id) => void
-}
+  chatData: any;
+  handleGetMessagesData: (id) => void;
+};
 
-export default function ListConversation({ chatData, handleGetMessagesData }: Props) {
+export default function ListConversation({
+  chatData,
+  handleGetMessagesData,
+}: Props) {
   return (
-    <Grid container className="list-conversation d-flex flex-column align-items-center h-100">
-      <Grid item className="list-conversation-header w-100 d-flex align-items-center">
-        <h2 className="f-30 f-inter dark-contrast-color fw-bold " style={{ padding: 0, margin: 0 }}>
+    <Grid
+      container
+      className="list-conversation d-flex flex-column align-items-center h-100"
+    >
+      <Grid
+        item
+        justifyContent="space-between"
+        sx={{height: '72px'}}
+        className="list-conversation-header w-100 d-flex align-items-center justi"
+      >
+        <h2
+          className="f-24 f-inter dark-contrast-color fw-bold "
+          style={{ padding: 0, margin: 0 }}
+        >
           Conversas
         </h2>
+        <Avatar
+          sx={{
+            width: 50,
+            height: 50,
+            bgcolor: "#274C77",
+            border: "4px solid white",
+          }}
+          alt={"namePhoto"}
+          src={`data:image/png;base64,`}
+        />
       </Grid>
-      <Grid item className="list-conversation-content d-flex flex-column align-items-center" style={{ gap: "0.5rem" }}>
+      <Grid
+        item
+        className="list-conversation-content d-flex flex-column align-items-center"
+        style={{ gap: "0.5rem", padding: '10px', padding }}
+      >
         {chatData.map((localData: any) => (
-          <Conversation handleGetMessagesData={handleGetMessagesData} localData={localData} />
+          <Conversation
+            handleGetMessagesData={handleGetMessagesData}
+            localData={localData}
+          />
         ))}
       </Grid>
     </Grid>
-  )
+  );
 }
